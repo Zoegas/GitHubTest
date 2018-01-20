b5c5b03b9df994a5982163e49a98cdf27ac47475
Zoegas <35398039+Zoegas@users.noreply.github.com>
2018-01-20 12:40:52 +0100

    Person test osv

--- testGitHub/testGitHub/Program.cs
+++ testGitHub/testGitHub/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace testGitHub
 {
+    public class Person
+    {
+        private String name;
+        private int lenght;
+
+        public void SetName(String NewName)
+        {
+            name = NewName;
+        }
+        public String GetName()
+        {
+            return name;
+        }
+        public void SetLenght (int NewLenght)
+        {
+            lenght = NewLenght;
+        }
+        public int GetLenght()
+        {
+            return lenght;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Person p = new Person();
+
+            p.SetLenght(190);
+            p.SetName("Viktor");
+
+            Console.WriteLine(p.GetName() + " " + p.GetLenght());
+            Console.Read();
         }
     }
 }
